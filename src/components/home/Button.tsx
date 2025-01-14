469c1d60e2dace958f7d5de15b108412cbb727f2
Mostafa <mostafa6320005@gmail.com>
2025-01-14 21:12:47 +0200

    fix error in style Button component in home page

--- src/components/home/Button.tsx
+++ src/components/home/Button.tsx
@@ -9,8 +9,8 @@ interface IProps {
 const Button = ({ primary, className, children }: IProps) => {
   return (
     <MButton
-      className={`bg-btn-${primary ? "primary" : "secondary"} hover:bg-btn-${
-        primary ? "primary" : "secondary"
+      className={`${primary ? "bg-btn-primary" : "bg-btn-secondary"} ${
+        primary ? "hover:bg-btn-primary-hover" : "hover:bg-btn-secondary-hover"
       }-hover p-0 rounded-xl transition-all duration-300 lg:w-[350px] ${className}`}
     >
       {children}
