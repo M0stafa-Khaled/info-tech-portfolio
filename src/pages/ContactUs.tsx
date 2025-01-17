6e11730228d249e4fbee32f4f0d5b446be603b33
Mostafa Khaled <mostafa6320005@gmail.com>
2025-01-17 23:02:16 +0200

    refactor ContactUs page and improve styling

--- src/pages/ContactUs.tsx
+++ src/pages/ContactUs.tsx
@@ -1,101 +1,163 @@
-import { FaPhone, FaEnvelope, FaMapMarkerAlt } from 'react-icons/fa';
+import { FaPhone, FaEnvelope, FaMapMarkerAlt } from "react-icons/fa";
+import BgImage from "../components/BgImage";
+import {
+  Button,
+  Input,
+  Radio,
+  Textarea,
+  Typography,
+} from "@material-tailwind/react";
+import { FiCheck } from "react-icons/fi";
 
-function ContactUs() {
+const ContactUs = () => {
   return (
-    <>
-      <div className="contact_title p-9 text-center mb-8 mt-2 text-white">
+    <section className="container">
+      <BgImage />
+      <div className="text-center my-9 lg:mt-[72px] text-white">
         <h1 className="text-4xl font-bold">Contact Us</h1>
-        <p className="text-lg p-4 text-gray-400">Any question or remarks? Just write us a message!</p>
+        <p className="text-lg p-4 text-muted">
+          Any question or remarks? Just write us a message!
+        </p>
       </div>
-
-      <div className="container bg-background-gradient p-4 rounded-xl mx-auto bg-transparent text-white w-full md:w-10/12">
-        <div className="card flex flex-col lg:flex-row gap-6">
+      <div className="bg-background-gradient rounded-2xl lg:rounded-3xl text-white w-full py-6 px-4 lg:px-6">
+        <div className="flex flex-col xl:flex-row gap-x-12 xl:gap-x-16 gap-y-12">
           {/* Left Card */}
-          <div className="card_left bg-[#0F1524] w-full lg:w-1/3 p-7 rounded-lg">
-            <h3 className="text-2xl font-semibold mb-4">Contact Information</h3>
-            <p className="text-gray-300 mb-9">You can contact us using this<br />information</p>
-            <div className="contact_icons space-y-4">
-              <div className="flex items-center gap-3 text-xs">
-                <FaPhone size={15} /> <span>+123 456 789</span>
+          <div className="bg-dark space-y-4 w-auto lg:min-w-max py-4 px-4 md:px-6 rounded-2xl">
+            <div className="px-4 py-3 space-y-2">
+              <h3 className="text-xl md:text-[28px] font-semibold">
+                Contact Information
+              </h3>
+              <p className="text-muted md:text-lg">
+                You can contact us using this information
+              </p>
+            </div>
+            <div className="px-4 py-3 space-y-5">
+              <div className="flex items-center gap-3 text-sm md:text-lg">
+                <span>
+                  <FaPhone size={24} />
+                </span>
+                <span>+123 456 789</span>
               </div>
-              <div className="flex items-center gap-3 text-xs">
-                <FaEnvelope size={15} /> <span>demo@gmail.com</span>
+              <div className="flex items-center gap-3 text-sm md:text-lg">
+                <span>
+                  <FaEnvelope size={24} />
+                </span>
+                <span>demo@gmail.com</span>
               </div>
-              <div className="flex items-center gap-3 text-xs">
-                <FaMapMarkerAlt size={15} /> <span>123 Dartmouth Street, Boston.</span>
+              <div className="flex items-center gap-3 text-sm md:text-lg">
+                <span>
+                  <FaMapMarkerAlt size={24} />
+                </span>
+                <span>123 Dartmouth Street, Boston.</span>
               </div>
             </div>
           </div>
 
           {/* Middle Card */}
-          <div className="card_middle w-full lg:w-2/3 p-6 rounded-lg flex flex-col gap-6">
-            {/* Inputs Row 1 */}
-            <div className="flex flex-col md:flex-row gap-4">
-              <input
-                type="text"
-                placeholder="First Name"
-                className="w-full p-3 border-b border-gray-600 bg-transparent text-white text-xs focus:outline-none focus:border-blue-400"
-              />
-              <input
-                type="text"
-                placeholder="Last Name"
-                className="w-full p-3 border-b border-gray-600 bg-transparent text-white text-xs focus:outline-none focus:border-blue-400"
-              />
-            </div>
+          <form
+            className="w-full rounded-lg flex flex-col gap-y-6"
+            onSubmit={(e) => e.preventDefault()}
+          >
+            {/* Inputs */}
+            <div className="flex flex-col gap-6">
+              {/* Inputs Row 1 */}
+              <div className="flex flex-col md:flex-row gap-4">
+                <Input
+                  variant="standard"
+                  label="First Name"
+                  className="text-white !border-muted focus:!border-muted after:!bg-muted min-w-fit"
+                  labelProps={{
+                    className:
+                      "!text-muted focus:!text-white after:!border-muted",
+                  }}
+                />
 
-            {/* Inputs Row 2 */}
-            <div className="flex flex-col md:flex-row gap-4">
-              <input
-                type="email"
-                placeholder="Email"
-                className="w-full p-3 border-b border-gray-600 bg-transparent text-white text-xs focus:outline-none focus:border-blue-400"
-              />
-              <input
-                type="tel"
-                placeholder="Phone Number"
-                className="w-full p-3 border-b border-gray-600 bg-transparent text-white text-xs focus:outline-none focus:border-blue-400"
-              />
+                <Input
+                  variant="standard"
+                  label="Last Name"
+                  className="text-white !border-muted"
+                  labelProps={{
+                    className:
+                      "!text-muted focus:!text-white after:!border-muted",
+                  }}
+                />
+              </div>
+              {/* Inputs Row 2 */}
+              <div className="flex flex-col md:flex-row gap-4">
+                <Input
+                  variant="standard"
+                  label="Email"
+                  className="text-white !border-muted"
+                  labelProps={{
+                    className:
+                      "!text-muted focus:!text-white after:!border-muted",
+                  }}
+                />
+
+                <Input
+                  variant="standard"
+                  label="Phone"
+                  className="text-white !border-muted"
+                  labelProps={{
+                    className:
+                      "!text-muted focus:!text-white after:!border-muted",
+                  }}
+                />
+              </div>
             </div>
 
             {/* Options */}
             <div>
-              <p className="mb-2 text-xs">Select Options</p>
-              <div className="flex flex-wrap gap-3 ">
-                {[["General", "Technical"], ["Feedback", "Other"]].map((row, rowIndex) => (
-                  <div key={rowIndex} className="flex gap-8  ">
-                    {row.map((option, index) => (
-                      <label key={index} className="flex items-center text-xs  ">
-                        <input
-                          type="radio"
-                          name="option"
-                          value={option.toLowerCase().replace(' ', '_')}
-                          className="w-3 h-3 border border-gray-600 rounded-full bg-transparent text-white focus:outline-none focus:ring-2 focus:ring-blue-400 mr-2"
-                        />
-                        {option}
-                      </label>
-                    ))}
-                  </div>
-                ))}
+              <p className="mb-4 text-lg">Select Options</p>
+              <div className="flex justify-between flex-wrap gap-3 ">
+                {["General", "Technical", "Feedback", "Other"].map(
+                  (option, index) => (
+                    <div key={index} className="flex gap-8 min-w-36">
+                      <Radio
+                        name="subject"
+                        ripple={false}
+                        icon={<FiCheck size={14} />}
+                        className="border-white bg-white p-0 transition-all hover:before:opacity-0"
+                        label={
+                          <Typography className="font-normal text-muted">
+                            {option}
+                          </Typography>
+                        }
+                        value={option}
+                        onChange={(e) => {
+                          console.log(e.target.value);
+                        }}
+                      />
+                    </div>
+                  )
+                )}
               </div>
             </div>
-
-            {/* Message and Button */}
-            <div className="flex flex-col gap-4">
-              <p className=' text-xs'> Message</p>
-              <textarea
-                placeholder="Write your message.."
-                className="w-full p-1 mb-4 border-b border-gray-600 bg-transparent text-xs text-white focus:outline-none focus:border-blue-400"
-                rows="1"
-              ></textarea>
-              <button className="w-full lg:w-[200px] py-3 bg-[#0F1524] text-white font-normal rounded-md hover:bg-[#0F1524]">
-                Send Message
-              </button>
+            <div>
+              <Textarea
+                variant="standard"
+                label="Message"
+                className="text-white !border-muted h-[50px]"
+                labelProps={{
+                  className:
+                    "peer-focus:after:!border-muted !text-muted focus:!text-white",
+                }}
+              />
             </div>
-          </div>
+            {/* Submit */}
+            <div>
+              <Button
+                type="submit"
+                className="w-full lg:w-[200px] normal-case py-4 bg-dark text-white text-base font-normal rounded-xl"
+              >
+                Send Message
+              </Button>
+            </div>
+          </form>
         </div>
       </div>
-    </>
+    </section>
   );
-}
+};
 
 export default ContactUs;
