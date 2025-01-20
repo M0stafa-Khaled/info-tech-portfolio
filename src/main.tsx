75fe7a3403fdd9f8cd392a949815ea376143af68
Mostafa Khaled <mostafa6320005@gmail.com>
2025-01-20 19:39:14 +0200

    feat: implement authentication slice and configure Redux store

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,11 @@ import App from "./App";
 import "swiper/css/bundle";
 import "swiper/css";
 import "./index.css";
+import { store } from "./app/store";
+import { Provider } from "react-redux";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <App />
+  <Provider store={store}>
+    <App />
+  </Provider>
 );
